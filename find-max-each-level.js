989ce3ca6a9e53a5ac5511a488b9046a5858226b
Stryker Huber <stryker.important@gmail.com>
2023-10-15 13:52:38 -0700

    1

--- find-max-each-level.js
+++ find-max-each-level.js
@@ -1,54 +1,58 @@
+// This is a class definition for a binary tree node
 class TreeNode {
   constructor(value, left, right) {
-    this.value = value;
-    this.left = left;
-    this.right = right;
+    this.value = value; // The value stored in the node
+    this.left = left; // The reference to the left child node
+    this.right = right; // The reference to the right child node
   }
 }
 
-// Given a tree, find the node with the highest value at each
-// level of the tree and return it in an array, with the root at the
-// 0th index, and the highest value in the deepest level of the tree
-// in the last index.
-
-//        5
-//       / \
-//      4   7
-//     / \   \
-//    1   3   2
-//       /    / \
-//      8    4   9
-//              / \
-//             2   4
-
-// Expected Output -> [ 5, 7, 3, 9, 4 ]
-
+// This function finds the largest value on each level of a binary tree
 function findMaxEachLevel(root) {
-  const stack = [];
-  const maxes = [];
 
-  root.level = 0;
+  // Initialize a stack with the root
 
-  while (stack.length > 0) {
-      const curr = stack.pop();
 
-      if (maxes[curr.level]) {
-          maxes[curr.level] = Math.min(curr.value, maxes[curr.level]);
-      } else {
-          maxes.push(curr.value);
-      }
+  // Initialize an empty array to store the max values on each level
 
-      if (curr.left && curr.left >= curr.right) {
-          curr.left.level = curr.level + 1;
-          stack.unshift(curr.left);
-      }
-      if (curr.right && curr.right >= curr.left) {
-          curr.right.level = curr.level + 1;
-          stack.unshift(curr.right);
-      }
-  }
 
-  return maxes;
+  // Print the initial stack and maxes for debugging purposes
+
+  // Assign a level property to the root node, starting from 0
+
+
+  // Loop until the stack is empty
+
+      // Pop the last element from the stack and assign it to curr
+
+
+      // Print the current node and the updated stack and maxes for debugging purposes
+
+
+      // Check if there is already a max value for the current level in the maxes array
+
+
+          // If yes, compare it with the current node's value and update it if needed
+
+          // If no, push the current node's value to the maxes array
+
+
+      // Check if the current node has a left child and if it is larger than or equal to its right child (optional condition)
+
+          // If yes, assign a level property to the left child, which is one more than the current node's level
+
+          // Push the left child to the front of the stack
+
+      // Check if the current node has a right child and if it is larger than or equal to its left child (optional condition)
+
+          // If yes, assign a level property to the right child, which is one more than the current node's level
+
+          // Push the right child to the front of the stack
+
+
+
+  // Return the maxes array as the final result
+
 }
 
 
